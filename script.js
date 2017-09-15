cb0a9cfaf8c8515caad5ab0f82cec46f9cb1c84e
George <george.ingram1@outlook.com>
2017-09-15 08:51:57 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var todoList = {
+  todos: [],
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+  }
+};
